379ce8ac38d43580d03b5ffad425d20b1e9900e2
halo51102 <20110667@student.hcmute.edu.vn>
2023-05-09 22:24:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dat_ve_xem_phim_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.dat_ve_xem_phim_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        deleteDatabase();
+    }
+}
